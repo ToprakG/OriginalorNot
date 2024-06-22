cb12a35009aee072cda87d575fdcc05ebc3bd962
ToprakG <74041550+ToprakG@users.noreply.github.com>
2024-06-22 13:36:09 +0300

    Update app.js

--- originalornot/app.js
+++ originalornot/app.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname));
 
 // Serve game.html on the root route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'game.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Serve leaderboard.html on the /leaderboard route
